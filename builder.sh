a8b672480fdb9d021e600dbcb0316cc35064037b
rcantabrana <rcantabrana@A0112110.dc.aragon.es>
2023-06-05 18:03:24 +0200

    using apk.... error

--- builder.sh
+++ builder.sh
@@ -2,6 +2,7 @@
 docker build --platform linux/amd64 https://github.com/rcgcoder/ubuntu-vnc.git -t rcgcoder/ubuntu-vnc:amd64
 docker push rcgcoder/ubuntu-vnc:amd64;
 sleep 10;
+
 docker manifest rm rcgcoder/ubuntu-vnc:manifest-latest;
 docker manifest create rcgcoder/ubuntu-vnc:manifest-latest --amend rcgcoder/ubuntu-vnc:latest --amend rcgcoder/ubuntu-vnc:amd64
 docker manifest push rcgcoder/ubuntu-vnc:manifest-latest
