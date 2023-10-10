98d3b6473fdcb3a7fb3db439c8f67142db684852
rcantabrana <rcantabrana@A0112195.dc.aragon.es>
2023-10-10 09:43:50 +0200

    new try

--- builder.sh
+++ builder.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker build --platform linux/amd64 https://github.com/rcgcoder/ubuntu-vnc.git -t rcgcoder/ubuntu-vnc:amd64
+docker build --no-cache --force-rm --platform linux/amd64 https://github.com/rcgcoder/ubuntu-vnc.git -t rcgcoder/ubuntu-vnc:amd64
 docker push rcgcoder/ubuntu-vnc:amd64;
 sleep 10;
 docker manifest rm rcgcoder/ubuntu-vnc:manifest-latest;
